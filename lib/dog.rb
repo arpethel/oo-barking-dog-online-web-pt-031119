2a5034873693dcf88528bfdfaf1afb733e1c2ae6
Ryan Pethel <arpethel@gmail.com>
2019-05-14 21:23:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 # Your code goes here!
 class Dog
+  attr_accessor :name
   fido = Dog.new
   #
-  #   fido.name = "Fido"
-  #
   fido.name = "Fido"
+  #
+  # fido.name = "Fido"
   # fido.name
 
 end
